a179876b550e7d50be4bae122ade2abc631d07cb
Christina Kayastha <ckayastha@vistaprint.com>
2016-01-30 15:53:51 -0500

    main screen sound

--- Assets/Scripts/LoadOnClick.cs
+++ Assets/Scripts/LoadOnClick.cs
@@ -4,6 +4,11 @@ using UnityEngine.SceneManagement;
 
 public class LoadOnClick : MonoBehaviour {
 
+	public void Start(){
+		var audioPlay = GetComponent<AudioSource>();
+		audioPlay.Play ();
+	}
+
 	public void LoadScene(int scene){
 		SceneManager.LoadScene (scene);
 	}
