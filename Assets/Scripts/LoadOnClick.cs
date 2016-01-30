8b214a28a3ea1f7d2ed807e97d474cfc7b6bca3c
Christina Kayastha <ckayastha@vistaprint.com>
2016-01-30 15:47:31 -0500

    Home Screen

---
+++ Assets/Scripts/LoadOnClick.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadOnClick : MonoBehaviour {
+
+	public void LoadScene(int scene){
+		SceneManager.LoadScene (scene);
+	}
+}
