f560d2b56f1780d3f24b5cd606b71f00d7e8599d
Don Griffin <donggriffin@Dons-MacBook-Pro-2.local>
2016-01-30 15:35:17 -0500

    Fixing lightsource, adding toecurl animation on input.

--- Assets/Scripts/CharacterBehavior.cs
+++ Assets/Scripts/CharacterBehavior.cs
@@ -4,17 +4,22 @@ using System.Collections;
 public class CharacterBehavior : MonoBehaviour {
 
 	// Use this for initialization
-
 	private Animator animator;
-	int idleHash = Animator.StringToHash("Idle");
+	private InputHandler inputHandler;
 
 	void Start () {
 		animator = GameObject.Find ("Character").GetComponentInChildren<Animator>();
+		inputHandler = GameObject.Find ("InputHandler").GetComponent<InputHandler> ();
+		inputHandler.ElementEvent += ToeCurl;
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void ToeCurl (ElementType element) {
+		animator.Play ("ToeCurl");
 	}
 }
