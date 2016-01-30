d0fda70a5f1796bccc7be90904192ea23e90776d
Don Griffin <donggriffin@Dons-MacBook-Pro-2.local>
2016-01-30 12:02:54 -0500

    Adding InputHandler, and Sphere example implementation.

--- Assets/Hop.cs
+++ Assets/Hop.cs
@@ -9,19 +9,48 @@ public class Hop : MonoBehaviour {
 	private AudioSource audioPlay;
 
 	private GameObject metroObject;
+
+	private GameObject inputHandler;
+
 	void Start () {
 		metroObject = GameObject.Find("Metronome");
+
 		rb = GameObject.Find("Sphere").AddComponent<Rigidbody>();
 		metroObject.GetComponent<Metronome>().OnTick += Talk;
 		metroObject.GetComponent<Metronome> ().OnNewMeasure += White;
 
 		audioPlay = GetComponent<AudioSource>();
 
+
+		//INPUTHANDLER EXAMPLE
+		inputHandler = GameObject.Find ("InputHandler");
+
+		inputHandler.GetComponent<InputHandler> ().FireEvent += Fire;
+		inputHandler.GetComponent<InputHandler> ().WaterEvent += Water;
+		inputHandler.GetComponent<InputHandler> ().AirEvent += Air;
+		inputHandler.GetComponent<InputHandler> ().EarthEvent += Earth;
+		inputHandler.GetComponent<InputHandler> ().OffBeatEvent += OffBeat;
 	}
 
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void Fire(InputHandler handler) {
+		print ("fireee");
+	}
+	void Water(InputHandler handler) {
+		print ("Wateeer");
+	}
+	void Air(InputHandler handler) {
+		print ("Aiiirr");
+	}
+	void Earth(InputHandler handler) {
+		print ("Eaaarth");
+	}
+	void OffBeat(InputHandler handler) {
+		print ("OFFBEAT");
 	}
 
 	void Talk(Metronome metro) {
--- Assets/InputHandler.cs
+++ Assets/InputHandler.cs
@@ -3,12 +3,19 @@ using System.Collections;
 
 public class InputHandler : MonoBehaviour {
 
-	// Use this for initialization
 	private System.DateTime lastBeat;
 
-	public double inputThreshold = .40;
+	public double inputThreshold = .35;
 
 	public Metronome metronome;
+
+	public delegate void InputEvent(InputHandler input);
+
+	public event InputEvent FireEvent;
+	public event InputEvent WaterEvent;
+	public event InputEvent EarthEvent;
+	public event InputEvent AirEvent;
+	public event InputEvent OffBeatEvent;
 
 	void Start () {
 		metronome = GameObject.Find ("Metronome").GetComponent<Metronome> ();
@@ -17,16 +24,22 @@ public class InputHandler : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetButtonDown("Horizontal") || Input.GetButtonDown("Vertical"))
-		{
-			System.DateTime inputTime = System.DateTime.Now;
-			double millisecondSpan = (inputTime - lastBeat).TotalMilliseconds;
-			if (System.Math.Round(millisecondSpan - inputThreshold) % 2 == 0 || System.Math.Round(millisecondSpan + inputThreshold) % 2 == 0) {
-				print ("onBeat" + System.Math.Round((inputTime - lastBeat).TotalMilliseconds));
-			} else {
-				print ("offBeat");
+		if (Input.GetKeyDown("w") && VerifyBeat ()) {
+			if (FireEvent != null) {
+				FireEvent (this);
 			}
-			//print ("fire");
+		} else if (Input.GetKeyDown ("s") && VerifyBeat ()) {
+			if (WaterEvent != null) {
+				WaterEvent (this);
+			}
+		} else if (Input.GetKeyDown("a") && VerifyBeat ()) {
+			if (AirEvent != null) {
+				AirEvent (this);
+			}
+		} else if (Input.GetKeyDown ("d") && VerifyBeat ()) {
+			if (EarthEvent != null) {
+				EarthEvent (this);
+			}
 		}
 	}
 
@@ -34,4 +47,20 @@ public class InputHandler : MonoBehaviour {
 		lastBeat = System.DateTime.Now;
 		//print (lastBeat);
 	}
+
+	bool VerifyBeat() {
+		System.DateTime inputTime = System.DateTime.Now;
+		double millisecondSpan = (inputTime - lastBeat).TotalMilliseconds;
+		if (System.Math.Round(millisecondSpan - inputThreshold) % 2 == 0 || System.Math.Round(millisecondSpan + inputThreshold) % 2 == 0) {
+			//print ("onBeat" + System.Math.Round((inputTime - lastBeat).TotalMilliseconds));
+			return true;
+		} else {
+			//print ("offBeat");
+			if (OffBeatEvent != null) {
+				OffBeatEvent (this);
+			}
+			return false;
+		}
+	}
+
 }
