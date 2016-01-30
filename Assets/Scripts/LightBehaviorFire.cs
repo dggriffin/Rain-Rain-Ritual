70fab7298029a00eba8c596a28b56cc9456de1ca
Don Griffin <donggriffin@Dons-MacBook-Pro-2.local>
2016-01-30 17:25:08 -0500

    Commiting fireplace behavior.

---
+++ Assets/Scripts/LightBehaviorFire.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightBehaviorFire : MonoBehaviour {
+
+	private Light thisLight;
+	private Color originalColor;
+	private float timePassed;
+	private float changedValue;
+	// Use this for initialization
+	void Start () {
+
+		thisLight = this.GetComponent<Light> ();
+
+		if (thisLight != null) {
+			originalColor = thisLight.color;
+		} else {
+			enabled = false;
+			return;
+		}
+
+		changedValue = 0;
+		timePassed = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timePassed = Time.time;
+		timePassed = timePassed - Mathf.Floor (timePassed);
+
+		thisLight.color = originalColor * CalculateChange ();
+	}
+
+	private float CalculateChange(){
+		changedValue = -Mathf.Sin (timePassed * 4 * Mathf.PI) * 0.08f + 0.95f;
+		return changedValue;
+	}
+}
