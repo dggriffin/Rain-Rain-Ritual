9f07e95e966daf9b16481e2261fc379dc3f5eb8d
Jeff Dougherty <dougherty.jeffrey@gmail.com>
2016-01-30 23:19:13 -0500

    When you lose, background music and dancing stops.

---
+++ Assets/Scripts/StopDancing.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StopDancing : MonoBehaviour {
+
+	public GameObject[] dancers;
+	AudioSource thisSource;
+	Animator[] dancerMotion;
+
+	// Use this for initialization
+	void Start () {
+		System.Array.Resize (ref dancerMotion, dancers.GetLength(0));
+		thisSource = gameObject.GetComponent<AudioSource> ();
+		for (int k = 0; k <= 3; k++) {
+			dancerMotion[k] = dancers[k].GetComponent<Animator>();
+		}
+	}
+
+	//TEST CODE ONLY
+	void Update(){
+		if (Input.GetKeyDown("q")){
+			StopMusic ();
+		}
+	}
+
+	void StopMusic(){
+		thisSource.Stop ();
+		for (int k = 0; k <= 3; k++) {
+			dancerMotion [k].enabled = false;
+		}
+	}
+}
