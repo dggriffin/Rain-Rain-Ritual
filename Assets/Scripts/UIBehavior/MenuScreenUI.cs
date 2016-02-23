7b8628d68d76d1191f7fda7dbe2671c21b5007a0
Jeff Dougherty <dougherty.jeffrey@gmail.com>
2016-02-23 17:59:27 -0500

    Outline of instructions screen for home scene

---
+++ Assets/Scripts/UIBehavior/MenuScreenUI.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MenuScreenUI : MonoBehaviour {
+
+	public GameObject helpScreen;
+
+	public void ShowHelp(){
+		helpScreen.SetActive (true);
+	}
+	
+	public void HideHelp(){
+		helpScreen.SetActive (false);
+	}
+}
