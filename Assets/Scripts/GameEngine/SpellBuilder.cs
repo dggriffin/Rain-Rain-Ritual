9fb06eeb3da2c3d0c34d1276d68aaff825530a71
Chris Kwan <chriswkwan@gmail.com>
2016-02-23 00:26:00 -0500

    Add win/lose stats

--- Assets/Scripts/GameEngine/SpellBuilder.cs
+++ Assets/Scripts/GameEngine/SpellBuilder.cs
@@ -30,6 +30,8 @@ public class SpellBuilder : MonoBehaviour {
 			//cwkTODO put back water and wind settings
 			//			new Element (ElementType.Water, 25, 32, 1.0f),
 			//			new Element (ElementType.Wind, 25, 32, 0.5f)
+
+			//cwkTODO easier settings for testing
 			new Element (ElementType.Water, 5, 10, 1.0f),
 			new Element (ElementType.Wind, 5, 10, 0.5f)
 		};
@@ -37,6 +39,8 @@ public class SpellBuilder : MonoBehaviour {
 		//cwkTODO put back spell
 		//		var rain = new Spell ("rain", elements, 20, 120,
 		//			audioDict.GetSound("thunderclap"), audioDict.GetSound("cloudfailure"));
+
+		//cwkTODO easier settings for testing
 		var rain = new Spell (name, elements, 5, 20,
 			audioDict.GetSound("thunderclap"), audioDict.GetSound("cloudfailure"));
 
@@ -79,18 +83,14 @@ public class SpellBuilder : MonoBehaviour {
 
 		if (nextSpell != null) {
 			nextSpell.OnStateChange += StartNextSpell;
-
 			nextSpell.StartSpell ();
+		} else {
+			EndGame ();
 		}
 	}
 
 	private Spell GetNextSpell () {
-		if (spellList == null || spellList.Count < 1) {
-			return null;
-		}
-
-		if (curSpellIndex >= spellList.Count) {
-			Debug.Log ("GAME OVER");
+		if (spellList == null || spellList.Count < 1 || curSpellIndex >= spellList.Count) {
 			return null;
 		}
 
@@ -99,5 +99,25 @@ public class SpellBuilder : MonoBehaviour {
 		curSpellIndex++;
 
 		return nextSpell;
+	}
+
+	private void EndGame () {
+		List<string> states = new List<string>();
+		int wins = 0;
+
+		foreach (var spell in spellList) {
+			states.Add (spell.State.ToString());
+			if (spell.State == SpellState.Win) {
+				wins++;
+			}
+		}
+
+		var gameResults = string.Format("{0}/{1}: ({2})",
+			wins,
+			spellList.Count,
+			string.Join(", ", states.ToArray())
+		);
+
+		Debug.Log ("GAME OVER: " + gameResults);
 	}
 }
