41133630a73fc4dfca856e4b117dfbf7ca36fa2a
Chris Kwan <chriswkwan@gmail.com>
2016-02-24 08:45:10 -0500

    Add some notes

--- Assets/Scripts/GameEngine/UserData.cs
+++ Assets/Scripts/GameEngine/UserData.cs
@@ -3,9 +3,22 @@ using System.Collections;
 
 public class UserData : MonoBehaviour {
 
+	//cwkTODO notes
+	//cwkTODO might have to do something like this: http://answers.unity3d.com/questions/610893/how-do-i-save-a-custom-class-of-variables-to-playe.html
+	//when a spell is won/lost
+	/*
+	 * RainSpell : { wins: 5, losses: 3 } //computed value: ratio: 5/8
+	 * if a spell is not there, add it
+	 * if a spell is not there, it means it is locked
+	 * if a spell is there, it means it is unlocked
+	 * /
+
 	// Use this for initialization
 	void Start () {
-	
+
+		//cwkTODO delete all to clean up once
+		//PlayerPrefs.SetString ("test", "hello world");
+		//var testVal = PlayerPrefs.GetString ("test");
 	}
 	
 	// Update is called once per frame
